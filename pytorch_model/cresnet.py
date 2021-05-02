0276a4cd3c19a805c1458e78149a8e5ae8093ceb
Shiqi Wang <tcwangshiqi@cs.columbia.edu>
2021-05-02 18:32:03 -0400

    pytorch model

---
+++ pytorch_model/cresnet.py
@@ -0,0 +1,254 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1, bn=True, kernel=3):
+        super(BasicBlock, self).__init__()
+        self.bn = bn
+        if kernel == 3:
+            # can only do planes 16, block1
+            self.conv1 = nn.Conv2d(
+                in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=(not self.bn))
+            if self.bn:
+                self.bn1 = nn.BatchNorm2d(planes)
+            self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                                   stride=1, padding=1, bias=(not self.bn))
+        elif kernel == 2:
+            # can do planes 32
+            self.conv1 = nn.Conv2d(
+                in_planes, planes, kernel_size=2, stride=stride, padding=1, bias=(not self.bn))
+            if self.bn:
+                self.bn1 = nn.BatchNorm2d(planes)
+            self.conv2 = nn.Conv2d(planes, planes, kernel_size=2,
+                                   stride=1, padding=0, bias=(not self.bn))
+        elif kernel == 1:
+            # can only do planes 16, block1
+            self.conv1 = nn.Conv2d(
+                in_planes, planes, kernel_size=1, stride=stride, padding=0, bias=(not self.bn))
+            if self.bn:
+                self.bn1 = nn.BatchNorm2d(planes)
+            self.conv2 = nn.Conv2d(planes, planes, kernel_size=1,
+                                   stride=1, padding=0, bias=(not self.bn))
+        else:
+            exit("kernel not supported!")
+
+        if self.bn:
+            self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            if self.bn:
+                self.shortcut = nn.Sequential(
+                    nn.Conv2d(in_planes, self.expansion*planes,
+                              kernel_size=1, stride=stride, bias=(not self.bn)),
+                    nn.BatchNorm2d(self.expansion*planes)
+                )
+            else:
+                self.shortcut = nn.Sequential(
+                    nn.Conv2d(in_planes, self.expansion*planes,
+                              kernel_size=1, stride=stride, bias=(not self.bn)),
+                )
+
+    def forward(self, x):
+        print("basic block")
+        if self.bn:
+            out = F.relu(self.bn1(self.conv1(x)))
+            print("residual relu:", out.shape, out[0].view(-1).shape)
+            out = self.bn2(self.conv2(out))
+        else:
+            out = F.relu(self.conv1(x))
+            print("residual relu:", out.shape, out[0].view(-1).shape)
+            out = self.conv2(out)
+        out += self.shortcut(x)
+        out = F.relu(out)
+        print("residual relu:", out.shape, out[0].view(-1).shape)
+        return out
+
+
+
+class CResNet5(nn.Module):
+    def __init__(self, block, num_blocks=2, num_classes=10, in_planes=64, bn=True, last_layer="avg"):
+        super(CResNet5, self).__init__()
+        self.in_planes = in_planes
+        self.bn = bn
+        self.last_layer = last_layer
+        self.conv1 = nn.Conv2d(3, in_planes, kernel_size=3,
+                               stride=2, padding=1, bias=not self.bn)
+        if self.bn: self.bn1 = nn.BatchNorm2d(in_planes)
+        self.layer1 = self._make_layer(block, in_planes*2, num_blocks, stride=2, bn=bn, kernel=3)
+        if self.last_layer == "avg":
+            self.avg2d = nn.AvgPool2d(4)
+            self.linear = nn.Linear(in_planes * 8 * block.expansion, num_classes)
+        elif self.last_layer == "dense":
+            self.linear1 = nn.Linear(in_planes * 8 * block.expansion * 16, 100)
+            self.linear2 = nn.Linear(100, num_classes)
+        else:
+            exit("last_layer type not supported!")
+
+    def _make_layer(self, block, planes, num_blocks, stride, bn, kernel):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride, bn, kernel))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        if self.bn:
+            out = F.relu(self.bn1(self.conv1(x)))
+        else:
+            out = F.relu(self.conv1(x))
+        print("conv1 relu", out.shape, out[0].view(-1).shape)
+        out = self.layer1(out)
+        print("layer1", out.shape)
+        if self.last_layer == "avg":
+            out = self.avg2d(out)
+            print("avg", out.shape)
+            out = out.view(out.size(0), -1)
+            print("view", out.shape)
+            out = self.linear(out)
+            print("output", out.shape)
+        elif self.last_layer == "dense":
+            out = out.view(out.size(0), -1)
+            print("view", out.shape)
+            out = F.relu(self.linear1(out))
+            print("linear1 relu", out.shape, out[0].view(-1).shape)
+            out = self.linear2(out)
+            print("output", out.shape)
+        return out
+
+
+class CResNet7(nn.Module):
+    def __init__(self, block, num_blocks=2, num_classes=10, in_planes=64, bn=True, last_layer="avg"):
+        super(CResNet7, self).__init__()
+        self.in_planes = in_planes
+        self.bn = bn
+        self.last_layer = last_layer
+        self.conv1 = nn.Conv2d(3, in_planes, kernel_size=3,
+                               stride=2, padding=1, bias=not self.bn)
+        if self.bn: self.bn1 = nn.BatchNorm2d(in_planes)
+        self.layer1 = self._make_layer(block, in_planes*2, num_blocks, stride=2, bn=bn, kernel=3)
+        self.layer2 = self._make_layer(block, in_planes*2, num_blocks, stride=2, bn=bn, kernel=3)
+        if self.last_layer == "avg":
+            self.avg2d = nn.AvgPool2d(4)
+            self.linear = nn.Linear(in_planes * 2 * block.expansion, num_classes)
+        elif self.last_layer == "dense":
+            self.linear1 = nn.Linear(in_planes * 2 * block.expansion * 16, 100)
+            self.linear2 = nn.Linear(100, num_classes)
+        else:
+            exit("last_layer type not supported!")
+
+    def _make_layer(self, block, planes, num_blocks, stride, bn, kernel):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride, bn, kernel))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        if self.bn:
+            out = F.relu(self.bn1(self.conv1(x)))
+        else:
+            out = F.relu(self.conv1(x))
+        print("conv1 relu", out.shape, out[0].view(-1).shape)
+        out = self.layer1(out)
+        print("layer1", out.shape)
+        out = self.layer2(out)
+        print("layer2", out.shape)
+        if self.last_layer == "avg":
+            out = self.avg2d(out)
+            print("avg", out.shape)
+            out = out.view(out.size(0), -1)
+            print("view", out.shape)
+            out = self.linear(out)
+            print("output", out.shape)
+        elif self.last_layer == "dense":
+            out = out.view(out.size(0), -1)
+            print("view", out.shape)
+            out = F.relu(self.linear1(out))
+            print("linear1 relu", out.shape, out[0].view(-1).shape)
+            out = self.linear2(out)
+            print("output", out.shape)
+        return out
+
+
+
+def cresnet5_16_dense_bn():
+    return CResNet5(BasicBlock, num_blocks=2, in_planes=16, bn=True, last_layer="dense")
+
+def cresnet5_16_avg_bn():
+    return CResNet5(BasicBlock, num_blocks=2, in_planes=16, bn=True, last_layer="avg")
+
+
+def cresnet5_8_dense_bn():
+    return CResNet5(BasicBlock, num_blocks=2, in_planes=8, bn=True, last_layer="dense")
+
+def cresnet5_8_avg_bn():
+    return CResNet5(BasicBlock, num_blocks=2, in_planes=8, bn=True, last_layer="avg")
+
+
+def cresnet5_4_dense_bn():
+    return CResNet5(BasicBlock, num_blocks=2, in_planes=4, bn=True, last_layer="dense")
+
+def cresnet5_4_avg_bn():
+    return CResNet5(BasicBlock, num_blocks=2, in_planes=4, bn=True, last_layer="avg")
+
+
+def cresnet7_8_dense_bn():
+    return CResNet7(BasicBlock, num_blocks=2, in_planes=8, bn=True, last_layer="dense")
+
+def cresnet7_8_avg_bn():
+    return CResNet7(BasicBlock, num_blocks=2, in_planes=8, bn=True, last_layer="avg")
+
+
+def cresnet7_4_dense_bn():
+    return CResNet7(BasicBlock, num_blocks=2, in_planes=4, bn=True, last_layer="dense")
+
+def cresnet7_4_avg_bn():
+    return CResNet7(BasicBlock, num_blocks=2, in_planes=4, bn=True, last_layer="avg")
+
+
+
+
+def cresnet5_16_dense():
+    return CResNet5(BasicBlock, num_blocks=2, in_planes=16, bn=False, last_layer="dense")
+
+def cresnet5_16_avg():
+    return CResNet5(BasicBlock, num_blocks=2, in_planes=16, bn=False, last_layer="avg")
+
+
+def cresnet5_8_dense():
+    return CResNet5(BasicBlock, num_blocks=2, in_planes=8, bn=False, last_layer="dense")
+
+def cresnet5_8_avg():
+    return CResNet5(BasicBlock, num_blocks=2, in_planes=8, bn=False, last_layer="avg")
+
+
+def cresnet5_4_dense():
+    return CResNet5(BasicBlock, num_blocks=2, in_planes=4, bn=False, last_layer="dense")
+
+def cresnet5_4_avg():
+    return CResNet5(BasicBlock, num_blocks=2, in_planes=4, bn=False, last_layer="avg")
+
+
+def cresnet7_8_dense():
+    return CResNet7(BasicBlock, num_blocks=2, in_planes=8, bn=False, last_layer="dense")
+
+def cresnet7_8_avg():
+    return CResNet7(BasicBlock, num_blocks=2, in_planes=8, bn=False, last_layer="avg")
+
+
+def cresnet7_4_dense():
+    return CResNet7(BasicBlock, num_blocks=2, in_planes=4, bn=False, last_layer="dense")
+
+def cresnet7_4_avg():
+    return CResNet7(BasicBlock, num_blocks=2, in_planes=4, bn=False, last_layer="avg")
